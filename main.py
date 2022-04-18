f033193f06f4662274db10c197e5006e5bbba462
FreakJazz <jazzrb2307@hotmail.com>
2022-04-17 20:57:16 -0500

    add air table and post method

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
 from typing import Optional
 from fastapi import FastAPI
 from airtable import airtable
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 
-at = airtable.Airtable('BASE_ID', 'API_KEY')
+AIRTABLE_BASE_ID=os.environ.get("BASE_ID")
+AIRTABLE_API_KEY=os.environ.get("API_KEY")
+AIRTABLE_TABLE_NAME=os.environ.get("TABLE_NAME")
+endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
 
-
-at.get('TABLE_NAME')
 
 app = FastAPI()
 
@@ -18,3 +22,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/api/post")
+def post_data(data):
+    print(data)
+
+    return {data}
+
+
